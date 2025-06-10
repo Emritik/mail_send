d04a8b4fddaa9262e3da44eaa3512d0d74532a1d
Ritik sharma <98308171+Emritik@users.noreply.github.com>
2025-06-10 14:54:11 +0530

    Update send_mail.php

--- backend/send_mail.php
+++ backend/send_mail.php
@@ -21,8 +21,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com'; 
             $mail->SMTPAuth = true;
-            $mail->Username = 'noreply.cogmac@gmail.com';
-            $mail->Password = 'ughzqlbdcseqxqsq';
+            $mail->Username = 'add your email ID';
+            $mail->Password = 'add your password';
             $mail->SMTPSecure = 'tls';
             $mail->Port = 587;
 
